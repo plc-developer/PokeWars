239f59c3566b073f002bc6d70aa19efbce3c5fc8
Washerker <philiplouis0717@gmail.com>
2022-06-16 16:42:04 +0800

    Added Pokecards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import PokeCards from "./PokeCards/PokeCards";
+
+const BodyContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%; 
+`;
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content:center;
+  max-width: 1140px;
+  width: 100%;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BodyContainer>
+      <AppContainer>
+        <h1>POKEWARS</h1>
+        <PokeCards />
+      </AppContainer>
+    </BodyContainer>
   );
 }
 
