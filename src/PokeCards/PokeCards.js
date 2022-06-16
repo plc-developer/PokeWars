077a94e6fa77cda341eef44761ee9e813092bc87
Washerker <philiplouis0717@gmail.com>
2022-06-16 23:48:32 +0800

    Finishing PokeCards

--- src/PokeCards/PokeCards.js
+++ src/PokeCards/PokeCards.js
@@ -10,9 +10,9 @@ const PokeCardContainer = styled.div`
     }
 
     background: #191c29;
-    width: calc(25rem/1.5);
-    height: 25rem;
-    padding: 3px;
+    width: calc(65vh/1.5);
+    height: 65vh;
+    padding: 8px;
     position: relative;
     border-radius: 6px;
     // justify-content: center;
@@ -42,7 +42,7 @@ const PokeCardContainer = styled.div`
     &::after{
         position: absolute;
         content: "";
-        top: calc(30rem/ / 6);
+        top: calc(65vh/ / 6);
         left: 0;
         right: 0;
         z-index: -1;
@@ -50,7 +50,7 @@ const PokeCardContainer = styled.div`
         width: 100%;
         margin: 0 auto;
         transform: scale(0.8);
-        filter: blur(calc(30rem/ / 6));
+        filter: blur(calc(65vh/ / 6));
         background-image: linear-gradient(
           var(--rotate)
           , #5ddcff, #3c67e3 43%, #4e00c2);
@@ -71,29 +71,50 @@ const PokeCardContainer = styled.div`
 `;
 
 const PokemonCardBody = styled.div`
-    border: 1px solid red;
     height: inherit;
     display: flex;
     flex-direction: column;
-`;
-
-const PokeNameContainer = styled.div`
-    border: 1px solid white;
-    color: white;
-    margin-top: auto;
-    width: 100%;
+    border-radius: 6px;
+    background: linear-gradient(0deg, rgba(25,28,41,1) 20%, rgba(196, 196, 164) 100%);
 `;
 
 const PokemonImageContainer = styled.div`
-    border: 1px solid violet;
+    display:flex;
+    justify-content: center;
     height: inherit;
+    padding: 20px;
+`;
+
+const PokemonInfoContainer = styled.div`
+    color: white;
+    margin-top: auto;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    h3 {
+        margin: 0;
+        :first-child{
+            text-transform: capitalize;
+        }
+    }
+`;
+
+const PokemonInfoTypes = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 10px;
 `;
 
 function PokeCards(){
     const [pokeName, setPokemonName] = useState('');
+
     const getPokemon = () => {
         Axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`)
         .then((res)=>{
+            console.log(res.data);
             setPokemonName(res.data.name);
         });
     }
@@ -106,11 +127,18 @@ function PokeCards(){
     <PokeCardContainer>
         <PokemonCardBody>
             <PokemonImageContainer>
-                <h1>IMAGE</h1>
+                <img src="https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/132.png" alt={pokeName} width="90%" height="90%" />
             </PokemonImageContainer>
-            <PokeNameContainer>
-                <h1>TEST</h1>
-            </PokeNameContainer>
+            <PokemonInfoContainer>
+                <h3>{pokeName}</h3>
+                <h3>#3122</h3>
+                <PokemonInfoTypes>
+                <svg width="20" height="20" fill="#3EC70B">
+                    <image fill="#3EC70B" xlinkHref="https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/Others/type-icons/bug.svg" src="yourfallback.png" width="20" height="20"/>  
+                </svg>
+                {/* <img style={{color: 'blue !important'}} src="https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/Others/type-icons/bug.svg" alt={pokeName} width="20" height="20" /> */}
+                </PokemonInfoTypes>
+            </PokemonInfoContainer>
         </PokemonCardBody>
         {/* <div style={{display: "flex", border: '1px solid red', height: '65vh', width: '65vh'}}>
             <div>
